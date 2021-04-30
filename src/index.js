d78d9323c142ea2d00e56365d43ec091ecd3ff6e
Richard Kwon <rkminjae@gmail.com>
2021-05-01 01:19:02 +1200

    Import fonts using webfontloader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import WebFont from 'webfontloader';
 import App from './App';
+
+WebFont.load({
+  google: {
+    families: ['Montserrat', 'Bitter', 'sans-serif', 'serif'],
+  },
+});
 
 // eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById('root'));
