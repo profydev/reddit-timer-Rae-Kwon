4ef81cb4b2498289867219e4fa9ff4de75da401f
Richard Kwon <rkminjae@gmail.com>
2021-05-02 20:19:46 +1200

    Create app skeleton (#1)
    * Add normalize.css to project
    
    * Install Bitter and Montserrat fonts
    
    * Add global stylesheet
    
    * Add react router to project
    
    * Add router with home and search routes
    
    * Remove typeface-montserrat and typeface-bitter as project was deprecated
    
    * Change li font-family to Montserrat
    
    * Import fonts using webfontloader
    
    * Change title to 'Reddit Timer'
    
    * Change short_name and name
    
    * Add Reddit Timer favicon
    
    * Add styled-components yarn package
    
    * Replace App.css stylesheet with styled-component ThemeProvider
    
    * Remove webfontloader
    
    * Add global styles to app
    
    * Add self-hosted fonts to global styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './GlobalStyle';
+import theme from './theme';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <GlobalStyle />
+        <div>
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/search">Search</Link></li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/" />
+            <Route path="/search" />
+          </Switch>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
