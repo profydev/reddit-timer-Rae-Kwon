361a11fe6bc7d5fa7cf9db806e2e0184091cde31
Richard Kwon <rkminjae@gmail.com>
2021-05-02 19:56:51 +1200

    Add global styles to app

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import {
   Link,
 } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './GlobalStyle';
 import theme from './theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Router>
+        <GlobalStyle />
         <div>
           <nav>
             <ul>
--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,19 @@
 const theme = {
-  main: 'red',
+  color: {
+    text: '#93918f',
+  },
+  font: {
+    family: {
+      default: '"Montserrat", sans-serif',
+      headline: '"Bitter", serif',
+    },
+    size: {
+      default: '16px',
+    },
+    lineHeight: {
+      default: 1.69,
+    },
+  },
 };
 
 export default theme;
